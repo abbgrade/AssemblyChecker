d83a1f01cc43492b1ab104e5b5581e788a4ba882
Steffen Kampmann <steffen.kampmann@gmail.com>
2023-02-16 20:56:40 +0100

    added parameters

--- .build.ps1
+++ .build.ps1
@@ -1,4 +1,19 @@
+
+param(
+	[string] $NuGetApiKey = $env:nuget_apikey,
+
+	# Overwrite published versions
+	[switch] $ForcePublish,
+
+    # Add doc templates for new command.
+	[switch] $ForceDocInit,
+
+	# Version suffix to prereleases
+	[int] $BuildNumber
+)
+
 $ModuleName = 'AssemblyChecker'
+
 . ./tasks/Build.Tasks.ps1
 
 task InstallBuildDependencies {
